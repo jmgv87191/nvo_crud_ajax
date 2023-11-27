f37637e439fe2c352bd89f92b590b68f8e2534d9
juan manuel <jmgv87191@gmail.com>
2023-11-26 19:06:41 -0700

    conexion exitosa

---
+++ conexion.php
@@ -0,0 +1,16 @@
+<?php
+
+    $servername = "localhost";
+    $user = "root";
+    $password = "";
+    $database = "";
+
+    $conn = mysqli_connect( $servername, $user, $password, $database );
+
+    if(!$conn){
+        die( "conexion sin exito". mysqli_connect_error() );
+    }
+
+    echo( "conexion exitosa" )
+
+?>
